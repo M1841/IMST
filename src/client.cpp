d8ca5cdfea723704acc5653550187b3e5f446e4d
M1841 <mihaimuresan592003@gmail.com>
2024-06-06 10:26:59 +0300

    Handle Multiple Requests

--- src/client.cpp
+++ src/client.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -6,46 +7,136 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static void die(const char *);
+const size_t MAX_MESSAGE_SIZE = 4096;
+
+static int32_t query(int, const char *);
+
+static int32_t read_full_message(int, char *, size_t);
+static int32_t write_full_message(int, const char *, size_t);
+
+static void exit_with_error(const char *);
+static void print_error_message(const char *);
 
 int main()
 {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0)
+  int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_socket < 0)
   {
-    die("socket()");
+    exit_with_error("socket()");
   }
 
-  struct sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_port = ntohs(8080);
-  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
+  struct sockaddr_in server_address = {};
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = ntohs(8080);
+  server_address.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
 
-  int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
-  if (rv)
+  int connection_status = connect(client_socket, (const struct sockaddr *)&server_address, sizeof(server_address));
+  if (connection_status)
   {
-    die("connect()");
+    exit_with_error("connect()");
   }
 
-  char msg[] = "hello";
-  write(fd, msg, strlen(msg));
-
-  char rbuf[64] = {};
-  ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-  if (n < 0)
+  int32_t error_code = query(client_socket, "hello1");
+  if (!error_code)
   {
-    die("read()");
+    error_code = query(client_socket, "hello2");
+    if (!error_code)
+    {
+      error_code = query(client_socket, "hello3");
+    }
   }
 
-  printf("server says: %s\n", rbuf);
-  close(fd);
-
+  close(client_socket);
   return 0;
 }
 
-static void die(const char *str)
+static int32_t query(int socket, const char *message)
 {
-  int err = errno;
-  fprintf(stderr, "[%d] %s\n", err, str);
+  uint32_t message_length = (uint32_t)strlen(message);
+  if (message_length > MAX_MESSAGE_SIZE)
+  {
+    return -1;
+  }
+
+  char write_buffer[4 + MAX_MESSAGE_SIZE];
+  memcpy(write_buffer, &message_length, 4);
+  memcpy(&write_buffer[4], message, message_length);
+
+  int32_t error_code = write_full_message(socket, write_buffer, 4 + message_length);
+  if (error_code)
+  {
+    return error_code;
+  }
+
+  char read_buffer[4 + MAX_MESSAGE_SIZE + 1];
+  errno = 0;
+
+  error_code = read_full_message(socket, read_buffer, 4);
+  if (error_code)
+  {
+    print_error_message(errno ? "read() error" : "EOF");
+    return error_code;
+  }
+
+  memcpy(&message_length, read_buffer, 4);
+  if (message_length > MAX_MESSAGE_SIZE)
+  {
+    print_error_message("too long");
+    return -1;
+  }
+
+  error_code = read_full_message(socket, &read_buffer[4], message_length);
+  if (error_code)
+  {
+    print_error_message("read() error");
+    return error_code;
+  }
+
+  read_buffer[4 + message_length] = '\0';
+  printf("server says: %s\n", &read_buffer[4]);
+  return 0;
+}
+
+static int32_t read_full_message(int socket, char *buffer, size_t num_bytes)
+{
+  while (num_bytes > 0)
+  {
+    ssize_t num_bytes_read = read(socket, buffer, num_bytes);
+    if (num_bytes_read <= 0)
+    {
+      return -1;
+    }
+    assert((size_t)num_bytes_read <= num_bytes);
+    num_bytes -= (size_t)num_bytes_read;
+    buffer += num_bytes_read;
+  }
+  return 0;
+}
+
+static int32_t write_full_message(int socket, const char *buffer, size_t num_bytes)
+{
+  while (num_bytes > 0)
+  {
+    ssize_t num_bytes_written = write(socket, buffer, num_bytes);
+    if (num_bytes_written <= 0)
+    {
+      return -1;
+    }
+    assert((size_t)num_bytes_written <= num_bytes);
+    num_bytes -= (size_t)num_bytes_written;
+    buffer += num_bytes_written;
+  }
+  return 0;
+}
+
+static void exit_with_error(const char *message)
+{
+  int error_code = errno;
+  fprintf(stderr, "[%d] %s\n", error_code, message);
   abort();
+}
+
+static void print_error_message(const char *message)
+{
+  fprintf(stderr, "%s\n", message);
 }
--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdbool.h>
@@ -7,80 +8,149 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-static void do_something(int);
-static void die(const char *);
-static void msg(const char *);
+const size_t MAX_MESSAGE_SIZE = 4096;
+
+static int32_t handle_single_request(int);
+
+static int32_t read_full_message(int, char *, size_t);
+static int32_t write_full_message(int, const char *, size_t);
+
+static void exit_with_error(const char *);
+static void print_error_message(const char *);
 
 int main()
 {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0)
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket < 0)
   {
-    die("socket()");
+    exit_with_error("socket()");
   }
 
-  int val = 1;
-  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+  int socket_option_value = 1;
+  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option_value, sizeof(socket_option_value));
 
-  struct sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_port = ntohs(8080);
-  addr.sin_addr.s_addr = ntohl(0);
+  struct sockaddr_in server_addr = {};
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = ntohs(8080);
+  server_addr.sin_addr.s_addr = ntohl(0);
 
-  int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
-  if (rv)
+  int bind_result = bind(server_socket, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (bind_result)
   {
-    die("bind()");
+    exit_with_error("bind()");
   }
 
-  rv = listen(fd, SOMAXCONN);
-  if (rv)
+  int listen_result = listen(server_socket, SOMAXCONN);
+  if (listen_result)
   {
-    die("listen()");
+    exit_with_error("listen()");
   }
 
   while (true)
   {
     struct sockaddr_in client_addr = {};
-    socklen_t addrlen = sizeof(client_addr);
+    socklen_t client_addr_length = sizeof(client_addr);
 
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-    if (connfd < 0)
+    int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_length);
+    if (client_socket < 0)
     {
       continue;
     }
 
-    do_something(connfd);
-    close(connfd);
+    while (true)
+    {
+      int32_t error_code = handle_single_request(client_socket);
+      if (error_code)
+      {
+        break;
+      }
+    }
+    close(client_socket);
   }
 
   return 0;
 }
 
-static void do_something(int connfd)
+static int32_t handle_single_request(int client_socket)
 {
-  char rbuf[64] = {};
+  char read_buffer[4 + MAX_MESSAGE_SIZE + 1];
+  errno = 0;
 
-  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-  if (n < 0)
+  int32_t error_code = read_full_message(client_socket, read_buffer, 4);
+  if (error_code)
   {
-    msg("read() error");
-    return;
+    print_error_message(errno ? "read() error" : "EOF");
+    return error_code;
   }
-  printf("client says: %s\n", rbuf);
 
-  char wbuf[] = "world";
-  write(connfd, wbuf, strlen(wbuf));
+  uint32_t message_length = 0;
+  memcpy(&message_length, read_buffer, 4);
+  if (message_length > MAX_MESSAGE_SIZE)
+  {
+    print_error_message("too long");
+    return -1;
+  }
+
+  error_code = read_full_message(client_socket, &read_buffer[4], message_length);
+  if (error_code)
+  {
+    print_error_message("read() error");
+    return error_code;
+  }
+
+  read_buffer[4 + message_length] = '\0';
+  printf("client says: %s\n", &read_buffer[4]);
+
+  const char reply[] = "world!";
+  char write_buffer[4 + sizeof(reply)];
+  message_length = (uint32_t)strlen(reply);
+
+  memcpy(write_buffer, &message_length, 4);
+  memcpy(&write_buffer[4], reply, message_length);
+
+  return write_full_message(client_socket, write_buffer, 4 + message_length);
 }
 
-static void die(const char *str)
+static int32_t read_full_message(int socket, char *buffer, size_t num_bytes)
 {
-  int err = errno;
-  fprintf(stderr, "[%d] %s\n", err, str);
+  while (num_bytes > 0)
+  {
+    ssize_t num_bytes_read = read(socket, buffer, num_bytes);
+    if (num_bytes_read <= 0)
+    {
+      return -1;
+    }
+    assert((size_t)num_bytes_read <= num_bytes);
+    num_bytes -= (size_t)num_bytes_read;
+    buffer += num_bytes_read;
+  }
+  return 0;
+}
+
+static int32_t write_full_message(int socket, const char *buffer, size_t num_bytes)
+{
+  while (num_bytes > 0)
+  {
+    ssize_t num_bytes_written = write(socket, buffer, num_bytes);
+    if (num_bytes_written <= 0)
+    {
+      return -1;
+    }
+    assert((size_t)num_bytes_written <= num_bytes);
+    num_bytes -= (size_t)num_bytes_written;
+    buffer += num_bytes_written;
+  }
+  return 0;
+}
+
+static void exit_with_error(const char *message)
+{
+  int error_code = errno;
+  fprintf(stderr, "[%d] %s\n", error_code, message);
   abort();
 }
 
-static void msg(const char *str)
+static void print_error_message(const char *message)
 {
-  fprintf(stderr, "%s\n", str);
+  fprintf(stderr, "%s\n", message);
 }
